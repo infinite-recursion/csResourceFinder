a110c6d76f2ac4126b976821f256cea812f2b2e7
Joshua Baxter <jbreezy248@gmail.com>
2020-07-02 00:37:28 -0400

    Added endpoint for retrieving all the existing tags.

---
+++ src/main/java/com/csresource/controller/TagController.java
@@ -0,0 +1,36 @@
+package com.csresource.controller;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.csresource.jpa.Tag;
+import com.csresource.repositories.TagRepository;
+
+@RestController
+public class TagController {
+
+	@Autowired
+	TagRepository tagRepo;
+
+	@GetMapping("/getTags")
+	public List<String> getTags() {
+
+		LinkedList<String> tags = new LinkedList<String>();
+
+		Iterable<Tag> tagsJpaList = tagRepo.findAll(Sort.by("name").ascending());
+
+		for (Tag tag : tagsJpaList) {
+			tags.add(tag.getName());
+		}
+
+		return tags;
+
+	}
+
+}
