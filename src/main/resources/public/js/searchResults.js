a27a4df24adb0c378a52edebe8c7a9d2f5497ae8
Joshua Baxter <jbreezy248@gmail.com>
2020-07-04 00:16:42 -0400

    Implemented search on the search results page

--- src/main/resources/public/js/searchResults.js
+++ src/main/resources/public/js/searchResults.js
@@ -3,7 +3,7 @@ var app = angular.module('app', []);
 
 // Define the `PhoneListController` controller on the `phonecatApp` module
 app.controller('SearchResultsController',
-		function SearchResultsController($scope, $http) {
+		function SearchResultsController($scope, $http, $rootScope) {
 
 	
 			//Get the search results if searched
@@ -30,5 +30,75 @@ app.controller('SearchResultsController',
 				window.location.href = '/resourceDetails.html';
 
 			}
+			
+			/**
+			 * Below is the logic for searching for results
+			 */
+			$scope.searchPriorityOptions = [];
+			var highestRating = {};
+			highestRating.value = 'highest rating';
+			highestRating.name = 'highest rating';
+			$scope.searchPriorityOptions.push(highestRating);
+			
+			var highestTag = {};
+			highestTag.value = 'highest tag';
+			highestTag.name = 'highest tag frequency';
+			$scope.searchPriorityOptions.push(highestTag);
+			
+			$scope.selectedSearchPriority = highestRating.value;
+			// Get the tags for the search
+			$http({
+				method : 'GET',
+				url : '/getTags'
+			}).then(function successCallback(response) {
+
+				$scope.tags = response.data;
+
+			}, function errorCallback(response) {
+				console.log("Error getting tags for search");
+			});
+			
+			$scope.search = function(){
+				
+				$scope.results.length = 0;
+				
+				var searchJson = {};
+				searchJson.keyword = $scope.searchKeyword;
+				searchJson.tag = $scope.selectedTag;
+				searchJson.searchPriority = $scope.selectedSearchPriority;
+				
+				
+				if($scope.selectedTag==null && searchJson.searchPriority=='highest tag'){
+					searchJson.searchPriority = null
+				}
+
+				// Perform the search
+				$http({
+					method : 'POST',
+					url : '/search',
+					data: searchJson
+				}).then(function successCallback(response) {
+
+					var searchResults = response.data;
+					//Refresh the search results
+					/*for(var i = 0; i < searchResults.length; i++){
+						
+						$scope.results.push(searchResults[i]);
+					}*/
+					
+					//$rootScope.$apply();
+					
+					localStorage.setItem("searchResults", JSON.stringify(searchResults));
+					
+					window.location.href = '/searchResults.html';
+
+
+				}, function errorCallback(response) {
+					console.log("Error performing search");
+				});
+				
+				
+			};
 
 		});
+
